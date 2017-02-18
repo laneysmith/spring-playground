52c9fa1378c6dad6d1ac4ba6f38430d94202e65d
Laney Smith <lsmith.unc@gmail.com>
2017-02-18 12:17:20 -0700

    complete Creating Endpoints assignment

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class HelloController {
     @GetMapping("/")
     public String helloWorld() {
         return "Hello!!";
+    }
+
+    @PostMapping("/stuff")
+    public String postStuff() {
+        return "you posted some stuff!!";
     }
 }
